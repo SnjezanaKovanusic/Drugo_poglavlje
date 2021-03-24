4560e0620dfe3c92fbf081a5414e51c9ff89e808
SnjezanaKovanusic <snjezanakovanusic@gmail.com>
2021-03-24 13:04:43 +0100

    prvi komit

---
+++ src/cetvrto_poglavlje/StingSaPraznimMjestom.java
@@ -0,0 +1,15 @@
+package cetvrto_poglavlje;
+
+import java.util.Scanner;
+
+public class StingSaPraznimMjestom {
+
+        public static void main(String[] args) {
+            Scanner scanner= new Scanner(System.in);
+            System.out.println("Unesite neki string: ");
+            String unos = scanner.next();
+            System.out.println(unos.replace("", " "));
+
+        }
+    }
+}
